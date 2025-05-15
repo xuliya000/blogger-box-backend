e4a2dba934e34aa972f4a1322183f16b54f02fcc
xuliya000 <154217998+xuliya000@users.noreply.github.com>
2025-05-15 14:28:52 +0200

    Add Swagger documentation to HelloWorldController

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -1,9 +1,22 @@
 package com.dauphine.blogger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+			title = "Blogger box backend",
+			description = "Blogger box endpoints and apis",
+			contact = @Contact(name = "Liya", email = "xuliya2001@gmail.com"),
+			version = "1.0.0"
+	)
+)
+
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -1,19 +1,40 @@
 package com.dauphine.blogger.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(
+    name = "Hello world API",
+    description = " My first hello world endpoints"
+)
 public class HelloWorldController {
 
-    @GetMapping("/hello-world")
+    @GetMapping("hello-world")
     public String helloWorld() {
         return "Hello World!";
     }
-    
-    @GetMapping("/hello")
-    public String hello(@RequestParam String name) {
+
+    @GetMapping("hello")
+    public String helloByName(@RequestParam String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("hello/{name}")
+    @Operation(
+        summary = "Hello by name endpoint",
+        description = "Returns 'Hello {name}' by path variable"
+    )
+    public String hello(
+        @Parameter(description = "Name to greet")
+        @PathVariable String name
+    ) {
         return "Hello " + name;
     }
 
